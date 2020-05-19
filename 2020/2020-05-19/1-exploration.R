6ba59bf96bb7d587c73ed0018dda3bd90eea38b0
amarsee <amarsee@live.com>
2020-05-19 07:21:22 -0500

    added May 19 2020

---
+++ 2020/2020-05-19/1-exploration.R
@@ -0,0 +1,11 @@
+# Volleyball data
+# From both AVP and FIVB international tournaments
+
+library(tidyverse)
+
+vb_matches <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-19/vb_matches.csv', guess_max = 76000)
+
+
+
+
+
