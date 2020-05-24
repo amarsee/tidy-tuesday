1dfb385a79ebf92fa91929de362073e725f031ca
amarsee <amarsee@live.com>
2020-05-24 16:13:01 -0500

    wins by year in avp

--- 2020/2020-05-19/1-exploration.R
+++ 2020/2020-05-19/1-exploration.R
@@ -9,5 +9,12 @@ summary(vb_matches)
 
 colSums(is.na(vb_matches))
 
+avp_winning_teams <- vb_matches %>% 
+  mutate(team_name = paste(w_player1, w_player2, sep = '/')) %>% 
+  filter(circuit == 'AVP') %>% 
+  group_by(team_name, year) %>% 
+  summarise(
+    n_wins = n()
+  )
 
 
