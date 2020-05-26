65596e1466cff79dc6183f555b90c60d15454587
amarsee <amarsee@live.com>
2020-05-26 13:20:31 -0500

    May 26, 2020

---
+++ 2020/2020-05-26/1_exploration.R
@@ -0,0 +1,9 @@
+# Cocktail data from kaggle
+# May 26, 2020 Tidy Tuesday data set
+
+library(tidyverse)
+
+cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/cocktails.csv')
+boston_cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/boston_cocktails.csv')
+
+
